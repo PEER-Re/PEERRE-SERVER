d2a6fa347be66914f1c19900dd70aa075a359496
hysong4u <hayeon_song@naver.com>
2024-02-07 04:00:47 +0900

    [feat] #9 프로젝트 생성 controller 구현

--- src/main/java/org/umc/peerre/domain/project/controller/ProjectController.java
+++ src/main/java/org/umc/peerre/domain/project/controller/ProjectController.java
@@ -1,11 +1,25 @@
 package org.umc.peerre.domain.project.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.umc.peerre.domain.project.dto.request.CreateProjectRequestDto;
+import org.umc.peerre.domain.project.service.ProjectService;
+import org.umc.peerre.global.common.SuccessResponse;
+import org.umc.peerre.global.config.auth.UserId;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/project")
 @RestController
 public class ProjectController {
+
+    private final ProjectService projectService;
+    @PostMapping
+    public ResponseEntity<SuccessResponse<?>> createProject(@RequestBody CreateProjectRequestDto createProjectRequestDto) {
+        final CreateProjectRequestDto newProject = projectService.createProject(createProjectRequestDto);
+        return SuccessResponse.created(newProject);
+    }
 }
