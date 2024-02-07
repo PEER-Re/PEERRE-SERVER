7e3b22d7950aa2250c2aa92952bf8ccc288cfb57
hysong4u <hayeon_song@naver.com>
2024-02-07 17:45:27 +0900

    [feat] #13 결과리포트(내가 받은 피드백) 조회 API Controller 구현

--- src/main/java/org/umc/peerre/domain/project/controller/ProjectController.java
+++ src/main/java/org/umc/peerre/domain/project/controller/ProjectController.java
@@ -5,9 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.umc.peerre.domain.project.dto.request.CreateProjectRequestDto;
 import org.umc.peerre.domain.project.dto.response.CreateProjectResponseDto;
+import org.umc.peerre.domain.project.dto.response.MyFeedbackResponseDto;
 import org.umc.peerre.domain.project.dto.response.TeamInfoResponseDto;
 import org.umc.peerre.domain.project.service.ProjectService;
 import org.umc.peerre.global.common.SuccessResponse;
+import org.umc.peerre.global.config.auth.UserId;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/project")
@@ -34,9 +38,15 @@ public class ProjectController {
         return SuccessResponse.ok(null);
     }
 
-    @GetMapping("/{projectId}/team-info")
+    @GetMapping("/{projectId}/project-info")
     public ResponseEntity<SuccessResponse<?>> getTeamInfo(@PathVariable Long projectId) {
         final TeamInfoResponseDto teamInfoResponseDto = projectService.getTeamInfo(projectId);
         return SuccessResponse.ok(teamInfoResponseDto);
+    }
+
+    @GetMapping("/{projectId}/my-feedback")
+    public ResponseEntity<SuccessResponse<?>> getMyFeedback(@UserId Long userId, @PathVariable Long projectId) {
+        final MyFeedbackResponseDto myFeedbackResponseDto = projectService.getMyFeedback(userId, projectId);
+        return SuccessResponse.ok(myFeedbackResponseDto);
     }
 }
