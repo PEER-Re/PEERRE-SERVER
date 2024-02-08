144c5282012f2fe88be9d6a40b9482f6f9a8af9d
mungsil <thddmd2009@gmail.com>
2024-02-09 02:40:03 +0900

    [feat] #33 회고 스크롤 조회 기능 구현

--- src/main/java/org/umc/peerre/domain/project/repository/CommentRepository.java
+++ src/main/java/org/umc/peerre/domain/project/repository/CommentRepository.java
@@ -1,8 +1,13 @@
 package org.umc.peerre.domain.project.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.parameters.P;
 import org.umc.peerre.domain.project.entity.Comment;
+import org.umc.peerre.domain.project.entity.Project;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
-
+    Page<Comment> findByProjectAndIdLessThanOrderById(Project project,Long id, Pageable pageable);
 }
