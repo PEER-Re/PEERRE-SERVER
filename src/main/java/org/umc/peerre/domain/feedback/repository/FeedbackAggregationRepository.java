982953ea51f085511e24d6d42975b657809ca8f8
DDeo99 <yoondukwoo@gmail.com>
2024-02-08 18:18:41 +0900

    [fix] #6 FeedbackAggregation 추가 생성 방지

--- src/main/java/org/umc/peerre/domain/feedback/repository/FeedbackAggregationRepository.java
+++ src/main/java/org/umc/peerre/domain/feedback/repository/FeedbackAggregationRepository.java
@@ -3,8 +3,13 @@ package org.umc.peerre.domain.feedback.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.umc.peerre.domain.feedback.entity.FeedbackAggregation;
+import org.umc.peerre.domain.project.entity.Project;
+import org.umc.peerre.domain.user.entity.User;
+
+import java.util.Optional;
 
 @Repository
 public interface FeedbackAggregationRepository extends JpaRepository<FeedbackAggregation,Long> {
     Integer countByProjectIdAndUserIdAndEvaluationStatus(Long userId, Long projectId, Boolean evaluationStatus);
+    Optional<FeedbackAggregation> findByUserAndProject(User user, Project project);
 }
