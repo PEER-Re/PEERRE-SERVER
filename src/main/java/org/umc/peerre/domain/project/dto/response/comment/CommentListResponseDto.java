7f4319358d1801046b9dc185aa3ddb22bebf47b4
mungsil <thddmd2009@gmail.com>
2024-02-11 00:05:25 +0900

    [feat] #33 반환 순서 뒤집기

--- src/main/java/org/umc/peerre/domain/project/dto/response/comment/CommentListResponseDto.java
+++ src/main/java/org/umc/peerre/domain/project/dto/response/comment/CommentListResponseDto.java
@@ -1,20 +1,27 @@
 package org.umc.peerre.domain.project.dto.response.comment;
 
 import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Slice;
 import org.umc.peerre.domain.project.entity.Comment;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Builder
+@Slf4j
 public record CommentListResponseDto(
         boolean isEmpty,
         int size,
         List<EachCommentResponseDto> commentList
 ) { public static CommentListResponseDto of(Slice<EachCommentResponseDto> comments) {
 
+    List<EachCommentResponseDto> responseDtos = new ArrayList<>(comments.getContent());
+    Collections.reverse(responseDtos);
+
     return CommentListResponseDto.builder()
             .isEmpty(comments.isEmpty())
             .size(comments.getNumberOfElements())
-            .commentList(comments.getContent()).build();
+            .commentList(responseDtos).build();
 }}
